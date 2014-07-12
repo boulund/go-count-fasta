610bc1dffb1b7703b7b9efe41f74a47f9d622057
Fredrik Boulund <fredrik.boulund@chalmers.se>
2014-07-12 16:18:02 +0200

    FASTA counter implemented. First try. Can't read long lines.

--- count_fasta.go
+++ count_fasta.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
-func main() {
-	fmt.Printf("Hello World.\n")
+func read_fasta(filename string) (int, float64) {
+	file, _ := os.Open(filename)
+	scanner := bufio.NewScanner(file)
+
+	numreq := 0
+	totlen := 0
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.HasPrefix(text, ">") {
+			numreq += 1
+		} else {
+			totlen += len(text)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return numreq, float64(totlen) / float64(numreq)
+
 }
 
+func main() {
+	numreq, avglen := read_fasta(os.Args[1])
+	fmt.Println(numreq, avglen)
+}
